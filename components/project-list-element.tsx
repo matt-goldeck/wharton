f1424a636791318c72b12dbe0911d0d0e29efd1e
Matt Goldeck <matt@goldeck.net>
2024-09-20 08:49:52 -0700

    font on project list display

--- components/project-list-element.tsx
+++ components/project-list-element.tsx
@@ -9,10 +9,14 @@ export function ProjectListElement({ project }: ProjectListElementProps) {
   return (
     <Link href={`/projects/${project.slug}`}>
       <div className="card bg-neutral max-w-full sm:max-w-md mx-auto transition-transform transform hover:scale-105 duration-300">
-        <div className="card-body items-center text-center">
-          <h2 className="card-title font-vt323 text-xl sm:text-2xl">{project.title}</h2>
-          <p className="text-xs sm:text-sm text-neutral-content">{project.timestamp}</p>
-          <p className="text-xs sm:text-sm text-neutral-content">{project.description}</p>
+        <div className="card-body items-center text-center font-vt323">
+          <h2 className="card-title text-x2l sm:text-3xl">{project.title}</h2>
+          <p className="text-lg sm:text-lg text-neutral-content">
+            {project.timestamp}
+          </p>
+          <p className="text-lg sm:text-lg text-neutral-content">
+            {project.description}
+          </p>
         </div>
       </div>
     </Link>
